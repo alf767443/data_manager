1c6babba51ab10592b6673e5caf249b71a52cafe
alf767443 <andreluisfranca98+GitHub@gmail.com>
2023-01-07 17:17:18 +0000

    First commit

--- src/local2cloud.py
+++ src/local2cloud.py
@@ -4,19 +4,34 @@
 # Global imports
 from GlobalSets.Mongo import Clients as MongoClient, DataBases as db, Collections as col
 
-import rospy, pymongo
+import rospy
 from std_msgs.msg import String
+
+def up2cloud():
+    #node = rospy.Publisher('Upload2Cloud', String, queue_size=1)
+    rospy.init_node('Upload2Cloud', anonymous=False)
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        print('Hello')
+
+
 
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+
     while not rospy.is_shutdown():
         teste = {
             "first": 1,
             "second": 'a'
         }
+
+
         result = MongoClient.LocalClient[db.dbBuffer][col.Battery].insert_one(teste)
+
+
         
         rospy.loginfo(str(result))
         pub.publish(str(result))
@@ -24,6 +39,6 @@ def talker():
 
 if __name__ == '__main__':
     try:
-        talker()
+        up2cloud()
     except rospy.ROSInterruptException:
         pass
