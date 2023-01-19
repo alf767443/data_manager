f9f60df4416d828a24aa7fbf696d36fcc3259aa9
alf767443 <andreluisfranca98@gmail.com>
2023-01-19 17:35:38 +0000

    Buffer manager

--- src/bufferManager.py
+++ src/bufferManager.py
@@ -1,38 +1,20 @@
-
-
-
-
 #!/usr/bin/env python3
 
 # Global imports
-from GlobalSets.Mongo import Clients as MongoClient, DataBases as db, Collections as col, log, delRandomItem, sizeOf
 from GlobalSets.localSave import getFiles
 
 import rospy
-from std_msgs.msg import String
-
-
-## What is the database path
-dataPath = {
-    'dataSource': Source.CeDRI_UGV, 
-    'dataBase': db.dbBuffer, 
-    'collection': col.Battery
-}
-
-# To fake data
-from random import random
-from datetime import datetime
-
 class bufferManager():
     def __init__(self) -> None:
         rospy.init_node('bufferManager', anonymous=False)
 
-        getFiles()
+        print(getFiles(), end='\r')
 
         rospy.spin()
 
 if __name__ == '__main__':
     try:
+        print("Is sync?")
         bufferManager()
     except rospy.ROSInterruptException:
         pass
