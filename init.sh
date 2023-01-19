ecec89176ba567cbfb87bb1766cefcba3fb933c8
alf767443 <andreluisfranca98@gmail.com>
2023-01-19 18:36:51 +0000

    init file

--- init.sh
+++ init.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Clean
+rosnode kill /getBattery
+rosnode kill /getMotors
+rosnode kill /getPosition
+rosnode kill /getPositionOdom
+rosnode kill /bufferManager
+
+
+
 #Run  gets
 rosrun data_manager getBattery.py &
 rosrun data_manager getMotor.py &
@@ -8,4 +17,5 @@ rosrun data_manager getPositionOdom.py &
 
 # Run buffer manager
 rosrun data_manager bufferManager.py
+
 
