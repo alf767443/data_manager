4b00f0c7db4485432b0f8dc7f498e9571a30c406
alf767443 <andreluisfranca98@gmail.com>
2023-01-19 18:24:32 +0000

    init file

--- init.sh
+++ init.sh
@@ -1,1 +1,11 @@
-source venv/bin/activate
+#!/usr/bin/env bash
+
+#Run  gets
+rosrun data_manager getBattery.py &
+rosrun data_manager getMotor.py &
+rosrun data_manager getPositionAMCL.py &
+rosrun data_manager getPositionOdom.py &
+
+# Run buffer manager
+rosrun data_manager bufferManager.py
+
