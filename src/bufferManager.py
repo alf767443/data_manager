ab62daf0ba0e768673583fb5030e5f6525cfb3e3
alf767443 <andreluisfranca98@gmail.com>
2023-01-19 17:32:21 +0000

    Buffer manager

--- src/bufferManager.py
+++ src/bufferManager.py
@@ -1,43 +1,38 @@
+
+
+
+
 #!/usr/bin/env python3
 
 # Global imports
 from GlobalSets.Mongo import Clients as MongoClient, DataBases as db, Collections as col, log, delRandomItem, sizeOf
-import GlobalSets.l2c_functions as l2c
+from GlobalSets.localSave import getFiles
 
 import rospy
 from std_msgs.msg import String
 
-def bufferSizeManager_local():
-    rospy.init_node('bufferSizeManager', anonymous=False)
-    rate = rospy.Rate(1) 
-    while not rospy.is_shutdown(): 
-        try:
-            mongodb_databases = [MongoClient.LocalClient]
-        except Exception as e:
-            eStr    = str(e)
-            rospy.loginfo(eStr) 
-            continue
-        try: 
-            for database in mongodb_databases:
-                for collection in col.Collections:
-                    mongodb_collection = database[db.dbBuffer][collection['name']] 
-                    delTry = 0
-                    while sizeOf(mongodb_collection) > collection['maxBufferSize'] and delTry < 50:
-                        delTry += 1
-                        try:
-                            delRandomItem(mongodb_collection)
-                        except Exception as e:
-                            eStr    = str(e)
-                            rospy.loginfo(eStr)
-                            continue            
-            rate.sleep()
-        except Exception as e:
-            eStr    = str(e)
-            rospy.loginfo(eStr)
-            continue
+
+## What is the database path
+dataPath = {
+    'dataSource': Source.CeDRI_UGV, 
+    'dataBase': db.dbBuffer, 
+    'collection': col.Battery
+}
+
+# To fake data
+from random import random
+from datetime import datetime
+
+class bufferManager():
+    def __init__(self) -> None:
+        rospy.init_node('bufferManager', anonymous=False)
+
+        getFiles()
+
+        rospy.spin()
 
 if __name__ == '__main__':
     try:
-        bufferSizeManager_local()
+        bufferManager()
     except rospy.ROSInterruptException:
         pass
