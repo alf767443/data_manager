16b960456b2a2c0244bb1fa184ed2a25df470a1d
alf767443 <andreluisfranca98@gmail.com>
2023-02-01 17:35:06 +0000

    Queue action

---
+++ src/queueActions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Global imports
+from GlobalSets.localSave import createFile
+from GlobalSets.util import msg_to_document
+
+import pymongo, bson, datetime
+from GlobalSets.Mongo import Clients as MongoClient, DataBases as db, Collections as col
+
+class listenNodes:
+    def __init__(self, NODES) -> None:
+        rospy.init_node('queueActions', anonymous=False)
+        self.getFromRemoteUnit()
+        rospy.spin()
+                       
+    def getFromRemoteUnit(self):
+        print('__________________')
+        print(list(MongoClient.RemoteUnitClient[db.dataLake]['Actions'].aggregate({})))
+
+        rate = rospy.Rate(1)
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        listenNodes()
+    except rospy.ROSInterruptException:
+        pass
