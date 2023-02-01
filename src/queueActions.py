7ae66496d2b527bc79da9c9186d84e8da57eaa0d
alf767443 <andreluisfranca98@gmail.com>
2023-02-01 19:56:43 +0000

    Queue action

--- src/queueActions.py
+++ src/queueActions.py
@@ -7,16 +7,37 @@ from GlobalSets.util import msg_to_document
 import pymongo, bson, datetime, rospy
 from GlobalSets.Mongo import Clients as MongoClient, DataBases as db, Collections as col
 
+
+pipeline = {
+    'Status_0|1' : [
+        {
+            '$match': {
+                'status': {
+                    '$lte': 1
+                }
+            }
+        }, {
+            '$sort': {
+                'dateTime': -1
+            }
+        }
+    ]
+}
+
+
 class listenNodes:
+    queue = []
+
     def __init__(self) -> None:
         rospy.init_node('queueActions', anonymous=False)
         self.getFromRemoteUnit()
         rospy.spin()
                        
     def getFromRemoteUnit(self):
-        print('__________________')
-        print(list(MongoClient.RemoteUnitClient[db.dataLake]['Actions'].aggregate([])))
-
+        actionsQueue = list(MongoClient.RemoteUnitClient[db.dataLake]['Actions'].aggregate(pipeline=pipeline['Status_0|1']))
+        for new in actionsQueue:
+            print(new)
+        print(actionsQueue)
         rate = rospy.Rate(1)
         rate.sleep()
 
