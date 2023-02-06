a2be4adb3bbb17bb2b9d12ed0bf2b8e3d3a3f486
alf767443 <andreluisfranca98@gmail.com>
2023-02-06 17:40:53 +0000

    *

--- src/queueActions.py
+++ src/queueActions.py
@@ -38,19 +38,15 @@ class listenNodes:
         rate = rospy.Rate(1)
         while not rospy.is_shutdown():
             self.getFromRemoteUnit()
-            try:
-                for action in list(filter(lambda d: d['status'] in [0], self.queue)):
-                    # Action run ok
-                    if self.runAction(action):
-                        # print(action)
-                        action.update({'status': 1})
-                    # Action failed
-                    else:
-                        # print(action)
-                        action.update({'status': 2})
-            except Exception as e:
-                print(e)
-                
+            for action in list(filter(lambda d: d['status'] in [0], self.queue)):
+                # Action run ok
+                if self.runAction(action):
+                    # print(action)
+                    action.update({'status': 1})
+                # Action failed
+                else:
+                    # print(action)
+                    action.update({'status': 2})
             # print(self.queue)
             updateMany(Client = MongoClient.RemoteUnitClient, dataPath = dataPath, content = self.queue)     
             rate.sleep()
@@ -62,7 +58,7 @@ class listenNodes:
         print(_id)
         print(list(MongoClient.RemoteUnitClient[db.dataLake]['Actions'].find({'_id': _id})))
         
-        self.queue.append(actionsQueue)
+        self.queue = actionsQueue
         
         
     def runAction(self, action):
