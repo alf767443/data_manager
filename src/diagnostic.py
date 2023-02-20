1d04f268df58e64325d605b921d1eab53bad02ce
alf767443 <andreluisfranca98@gmail.com>
2023-02-20 14:30:22 +0000

    Diagnostic node

--- src/diagnostic.py
+++ src/diagnostic.py
@@ -12,33 +12,34 @@ import rospy, bson
 from datetime import datetime
 from tf.transformations import euler_from_quaternion
 
-class listenNodes:
+class diagnosticsNodes:
     def __init__(self, NODES) -> None:
-        rospy.init_node('listenNodes', anonymous=False)
+        rospy.init_node('platform_diagnostics', anonymous=False)
         self.NODES = NODES
+        self.last_msg = {node['node']: None for node in NODES} 
         for node in self.NODES:
             self.newSubscriber(node=node)
         rospy.spin()
-               
+
     def newSubscriber(self, node):
         rospy.Subscriber(name='/' + node['node'], data_class=node['msg'], callback=self.callback, callback_args=node)
-        
+
     def callback(self, msg, args):
-        rate = rospy.Rate(args['rate'])
-        try:
-            data = msg_to_document(msg=msg)
-            data.update({'dateTime': datetime.now()})
-            ##
-            #print(data)
-            ##
-            createFile(dataPath=args['dataPath'], content=data) 
-        except Exception as e:
-            print(e)
+        print(msg)
+        if self.last_msg[args['node']] != msg:
+            rate = rospy.Rate(args['rate'])
+            try:
+                data = msg_to_document(msg=msg)
+                data.update({'dateTime': datetime.now()})
+                createFile(dataPath=args['dataPath'], content=data)
+                self.last_msg[args['node']] = msg
+            except Exception as e:
+                print(e)
         rate.sleep()
 
 
 if __name__ == '__main__':
     try:
-        listenNodes(NODES=DIAGNOSTICS_NODES)
+        diagnosticsNodes(NODES=DIAGNOSTICS_NODES)
     except rospy.ROSInterruptException:
         pass
