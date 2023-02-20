8c206a2d63ef4d74eef3db7daca419f8d3b696a5
alf767443 <andreluisfranca98@gmail.com>
2023-02-20 14:42:13 +0000

    *

--- src/nodes.py
+++ src/nodes.py
@@ -125,44 +125,44 @@ DIAGNOSTICS_NODES = [
             'collection': col.Diagnostics
         }
     }, 
-    {
-        'node'    : 'diagnostics_agg',
-        'msg'     : DiagnosticArray,
-        'rate'    : 1,
-        'dataPath': {
-            'dataSource': Source.CeDRI_UGV, 
-            'dataBase'  : db.dataLake,
-            'collection': col.Diagnostics
-        }
-    }, 
-    {
-        'node'    : 'motor_node/parameter_descriptions',
-        'msg'     : ConfigDescription,
-        'rate'    : 1,
-        'dataPath': {
-            'dataSource': Source.CeDRI_UGV, 
-            'dataBase'  : db.dataLake,
-            'collection': col.Diagnostics
-        }
-    }, 
-    {
-        'node'    : 'ubiquity_velocity_controller/parameter_descriptions',
-        'msg'     : ConfigDescription,
-        'rate'    : 1,
-        'dataPath': {
-            'dataSource': Source.CeDRI_UGV, 
-            'dataBase'  : db.dataLake,
-            'collection': col.Diagnostics
-        }
-    }, 
-    {
-        'node'    : 'urg_node/parameter_descriptions',
-        'msg'     : ConfigDescription,
-        'rate'    : 1,
-        'dataPath': {
-            'dataSource': Source.CeDRI_UGV, 
-            'dataBase'  : db.dataLake,
-            'collection': col.Diagnostics
-        }
-    }, 
+    # {
+    #     'node'    : 'diagnostics_agg',
+    #     'msg'     : DiagnosticArray,
+    #     'rate'    : 1,
+    #     'dataPath': {
+    #         'dataSource': Source.CeDRI_UGV, 
+    #         'dataBase'  : db.dataLake,
+    #         'collection': col.Diagnostics
+    #     }
+    # }, 
+    # {
+    #     'node'    : 'motor_node/parameter_descriptions',
+    #     'msg'     : ConfigDescription,
+    #     'rate'    : 1,
+    #     'dataPath': {
+    #         'dataSource': Source.CeDRI_UGV, 
+    #         'dataBase'  : db.dataLake,
+    #         'collection': col.Diagnostics
+    #     }
+    # }, 
+    # {
+    #     'node'    : 'ubiquity_velocity_controller/parameter_descriptions',
+    #     'msg'     : ConfigDescription,
+    #     'rate'    : 1,
+    #     'dataPath': {
+    #         'dataSource': Source.CeDRI_UGV, 
+    #         'dataBase'  : db.dataLake,
+    #         'collection': col.Diagnostics
+    #     }
+    # }, 
+    # {
+    #     'node'    : 'urg_node/parameter_descriptions',
+    #     'msg'     : ConfigDescription,
+    #     'rate'    : 1,
+    #     'dataPath': {
+    #         'dataSource': Source.CeDRI_UGV, 
+    #         'dataBase'  : db.dataLake,
+    #         'collection': col.Diagnostics
+    #     }
+    # }, 
 ]
