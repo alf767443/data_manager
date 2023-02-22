fdb08caad626c4d6efba07e2280240f494a4b993
alf767443 <andreluisfranca98@gmail.com>
2023-02-22 17:33:58 +0000

    *

--- src/getNodes.py
+++ src/getNodes.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python3
+
 import rospy
+import rosgraph
 import rosnode
-import xmlrpc.client
-
-def get_node_info(node_name):
-    # Get the URI and PID of the node
-    uri = xmlrpc.client.ServerProxy(rospy.get_master_uri()).lookupNode(node_name)
-    pid = xmlrpc.client.ServerProxy(uri).getPid('/')
-
-    return {'name': node_name, 'uri': uri, 'pid': pid}
 
 if __name__ == '__main__':
-    rospy.init_node('node_info')
-    nodes = rosnode.get_node_names()
-    for node in nodes:
-        info = get_node_info(node)
-        print("Node: " + info['name'])
-        print("  URI: " + info['uri'])
-        print("  PID: " + str(info['pid']))
-
-        # Get the topics that the node is publishing to
-        pubs, _ = rospy.get_published_topics(node)
-        print("  Published topics:")
-        for topic, msg_type in pubs:
-            print("    " + topic + " (" + msg_type + ")")
-
-        # Get the topics that the node is subscribed to
-        _, subs, _ = rospy.client.get_master().getSystemState()
-        node_subs = [x[0] for x in subs if node in x[1]]
-        print("  Subscribed topics:")
-        for topic in node_subs:
-            print("    " + topic)
+    rospy.init_node('topic_info')
+    
+    # get list of topics and their types
+    topics = rospy.get_published_topics()
+    
+    # print out the list of topics and their types
+    print('Topics:')
+    for topic in topics:
+        print(topic)
+        
+        # get list of nodes publishing and subscribing to this topic
+        publishers, subscribers = rosgraph.Master('/rostopic').getTopicTypes(topic[0])
+        nodes = publishers + subscribers
+        
+        # print out the list of nodes publishing and subscribing to this topic
+        for node in nodes:
+            print('  Node: ' + node)
+            
+            # get list of topics that this node is publishing to
+            pubs, _ = rospy.get_published_topics(node)
+            print('    Publishes:')
+            for pub in pubs:
+                print('      ' + pub[0])
+            
+            # get list of topics that this node is subscribing to
+            _, subs, _ = rospy.client.get_master().getSystemState()
+            node_subs = [x[0] for x in subs if node in x[1]]
+            print('    Subscribes:')
+            for sub in node_subs:
+                print('      ' + sub)
