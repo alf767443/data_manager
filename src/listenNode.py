adca9b4fb27843fe639ec10cac25daee6220cdd6
alf767443 <andreluisfranca98@gmail.com>
2023-02-22 11:23:42 +0000

    *

--- src/listenNode.py
+++ src/listenNode.py
@@ -17,14 +17,15 @@ class listenNodes:
         rospy.init_node('listenNodes', anonymous=False)
         self.NODES = NODES
         for node in self.NODES:
+            node['ROS_rate'] = rospy.Rate(node['rate'])
             self.newSubscriber(node=node)
         rospy.spin()
                
     def newSubscriber(self, node):
+        
         rospy.Subscriber(name='/' + node['node'], data_class=node['msg'], callback=self.callback, callback_args=node)
         
     def callback(self, msg, args):
-        rate = rospy.Rate(args['rate'])
         try:
             data = msg_to_document(msg=msg)
             data.update({'dateTime': datetime.now()})
@@ -43,7 +44,7 @@ class listenNodes:
             createFile(dataPath=args['dataPath'], content=data) 
         except Exception as e:
             print(e)
-        rate.sleep()
+        args['ROS_rate'].sleep()
 
 
 if __name__ == '__main__':
