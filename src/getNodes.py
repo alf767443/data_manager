0b93b15d2f89f05e07881175a827833a7721745c
alf767443 <andreluisfranca98@gmail.com>
2023-02-22 17:30:39 +0000

    *

--- src/getNodes.py
+++ src/getNodes.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
 import rospy
-import rosnode
+import xmlrpc.client
+
+def get_node_info(node_name):
+    # Get the URI and PID of the node
+    uri = xmlrpc.client.ServerProxy(rospy.get_master_uri()).lookupNode(node_name)
+    pid = xmlrpc.client.ServerProxy(uri).getPid('/')
+
+    return {'name': node_name, 'uri': uri, 'pid': pid}
 
 if __name__ == '__main__':
     rospy.init_node('node_info')
-    nodes = rosnode.get_node_names()
+    nodes = rospy.get_node_names()
     for node in nodes:
-        print("Node:", node)
-        node_info = rosnode.get_node_info(node)
-        print("  URI:", node_info.uri)
-        print("  PID:", node_info.pid)
-        published_topics = rospy.get_published_topics(node)
-        print("  Published Topics:")
-        for t in published_topics:
-            print("    *", t[0], "\tType:", t[1])
-            subscribers = rospy.get_subscriber_info(t[0])
-            print("    Subscribers:")
-            for s in subscribers:
-                print("      *", s[1], "\tType:", s[3])
+        info = get_node_info(node)
+        print("Node: " + info['name'])
+        print("  URI: " + info['uri'])
+        print("  PID: " + str(info['pid']))
+
+        # Get the topics that the node is publishing to
+        pubs, _ = rospy.get_published_topics(node)
+        print("  Published topics:")
+        for topic, msg_type in pubs:
+            print("    " + topic + " (" + msg_type + ")")
+
+        # Get the topics that the node is subscribed to
+        _, subs, _ = rospy.client.get_master().getSystemState()
+        node_subs = [x[0] for x in subs if node in x[1]]
+        print("  Subscribed topics:")
+        for topic in node_subs:
+            print("    " + topic)
