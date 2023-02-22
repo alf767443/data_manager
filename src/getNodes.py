d0e1d8e58f6ca104abf2825142756ca9e06aec17
alf767443 <andreluisfranca98@gmail.com>
2023-02-22 17:27:38 +0000

    *

--- src/getNodes.py
+++ src/getNodes.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
 import rospy
-import rosgraph
 
 if __name__ == '__main__':
     rospy.init_node('node_info')
-    nodes = rosgraph.Master('/rostopic').getSystemState()[0]
+    nodes = rospy.get_node_names()
     for node in nodes:
-        node_info = rosgraph.Master('/rostopic').lookupNode(node)
-        node_uri = node_info.uri
-        node_pid = node_info.pid
         print("Node:", node)
-        print("URI:", node_uri)
-        print("PID:", node_pid)
-        published_topics = rospy.get_published_topics(node)
-        print("Published Topics:")
-        for t in published_topics:
-            print("  *", t)
-        for t in published_topics:
-            subscribers = rospy.get_subscriber_info(t[0])
-            print("Subscribers for", t[0], ":")
-            for s in subscribers:
-                print("  *", s[1], "connected to", s[0])
+        published_topics = rospy.Publisher.get_num_connections(rospy.Publisher("test_topic", rospy.Empty, queue_size=1)._impl.tcpros_pub)
+        print("  Published Topics:", published_topics)
+        subscriptions = rospy.Subscriber.get_num_connections(rospy.Subscriber("test_topic", rospy.Empty)._impl.tcpros_sub)
+        print("  Subscriptions:", subscriptions)
