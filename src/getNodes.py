022f09134c5b99e27db3c1e139a102b952f63d9d
alf767443 <andreluisfranca98@gmail.com>
2023-02-22 17:40:38 +0000

    *

--- src/getNodes.py
+++ src/getNodes.py
@@ -1,36 +1,16 @@
 #!/usr/bin/env python3
-import rospy
-import rosgraph
-import rosnode
 
-if __name__ == '__main__':
-    rospy.init_node('topic_info')
-    
-    # get list of topics and their types
-    topics = rospy.get_published_topics()
-    
-    # print out the list of topics and their types
-    print('Topics:')
-    for topic in topics:
-        print(topic)
-        
-        # get list of nodes publishing and subscribing to this topic
-        publishers, subscribers = rosgraph.Master('/rostopic').getTopicTypes(topic[0])
-        nodes = publishers + subscribers
-        
-        # print out the list of nodes publishing and subscribing to this topic
-        for node in nodes:
-            print('  Node: ' + node)
-            
-            # get list of topics that this node is publishing to
-            pubs, _ = rospy.get_published_topics(node)
-            print('    Publishes:')
-            for pub in pubs:
-                print('      ' + pub[0])
-            
-            # get list of topics that this node is subscribing to
-            _, subs, _ = rospy.client.get_master().getSystemState()
-            node_subs = [x[0] for x in subs if node in x[1]]
-            print('    Subscribes:')
-            for sub in node_subs:
-                print('      ' + sub)
+import rospy
+import subprocess
+
+# Inicialize o nó do ROS
+rospy.init_node('my_node')
+
+# Obtenha a lista de nós em execução
+nodes = subprocess.check_output(['rosnode', 'list']).decode().splitlines()
+
+# Para cada nó em execução, obtenha as conexões
+for node in nodes:
+    connections = subprocess.check_output(['rosnode', 'info', node]).decode().splitlines()
+    print('Node:', node)
+    print('Connections:', connections)
