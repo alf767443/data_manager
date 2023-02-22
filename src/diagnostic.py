c7a2f0c41dec398ef946be4d2112309de1cad327
alf767443 <andreluisfranca98@gmail.com>
2023-02-22 14:25:32 +0000

    *

--- src/diagnostic.py
+++ src/diagnostic.py
@@ -52,37 +52,6 @@ class diagnosticsNodes:
         rate.sleep()
 
 
-
-    # def diff_dicts(self, dict1, dict2):
-    #     try:
-    #         diff = {}
-    #         for chave, valor in dict1.items():
-    #             if chave not in dict2:
-    #                 diff[chave] = valor
-    #             elif isinstance(valor, dict):
-    #                 sub_diff = self.diff_dicts(valor, dict2[chave])
-    #                 if sub_diff:
-    #                     diff[chave] = sub_diff
-    #             elif isinstance(valor, list):
-    #                 if len(valor) != len(dict2[chave]):
-    #                     diff[chave] = (valor, dict2[chave])
-    #                 else:
-    #                     sub_diffs = []
-    #                     for i in range(len(valor)):
-    #                         sub_diff = self.diff_dicts(valor[i], dict2[chave][i])
-    #                         if sub_diff:
-    #                             sub_diffs.append(sub_diff)
-    #                     if sub_diffs:
-    #                         diff[chave] = sub_diffs
-    #             elif valor != dict2[chave]:
-    #                 diff[chave] = (valor, dict2[chave])
-    #         for chave, valor in dict2.items():
-    #             if chave not in dict1:
-    #                 diff[chave] = valor
-    #         return diff
-    #     except:
-    #         pass
-
 if __name__ == '__main__':
     try:
         diagnosticsNodes(NODES=DIAGNOSTICS_NODES)
