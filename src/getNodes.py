477d72de4fea1a7032b7c39c4899e0ee9febb0dd
alf767443 <andreluisfranca98@gmail.com>
2023-02-24 10:17:10 +0000

    Nós + Mensagens

--- src/getNodes.py
+++ src/getNodes.py
@@ -2,7 +2,9 @@
 
 import rospy
 
+#Obtem os nós em execução e o tipo de mensagem que este publica
 if __name__ == '__main__':
     rospy.init_node('node_info')
     node_names = rospy.get_published_topics()
     print("Nodes and Topics currently running:\n{}".format("\n".join([str(x) for x in node_names])))
+
