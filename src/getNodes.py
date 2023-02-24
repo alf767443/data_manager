8f9fc0a4de40e8055ad21ca1f03d998ffe0a0aad
alf767443 <andreluisfranca98@gmail.com>
2023-02-24 10:02:25 +0000

    *

--- src/getNodes.py
+++ src/getNodes.py
@@ -2,19 +2,18 @@
 
 import rospy
 import rosnode
-from rosgraph import Master, Node
 
 if __name__ == '__main__':
     rospy.init_node('node_info')
-    master = Master('/rostopic')
     node_names = rosnode.get_node_names()
     print("Nodes currently running:\n{}".format("\n".join(node_names)))
 
     for node_name in node_names:
         try:
-            node_info = Node(node_name, master)
-            print("Node name: {}\nURI: {}\nXML-RPC URI: {}\nPublications: {}\nSubscriptions: {}\nServices: {}\n"
-                  .format(node_name, node_info.uri, node_info.xmlrpc_uri, node_info.publications,
-                          node_info.subscriptions, node_info.services))
+            node_api = rosnode.get_api_uri(rospy.get_master(), node_name)
+            node_info = rosnode.get_node_info(node_name)
+            print("Node name: {}\nAPI URI: {}\nPublications: {}\nSubscriptions: {}\nServices: {}\n"
+                  .format(node_name, node_api, node_info.get('publications'), node_info.get('subscriptions'),
+                          node_info.get('services')))
         except:
             pass
