0341c7391b65ba863c0a4cb10e09087a25e79f5d
alf767443 <andreluisfranca98@gmail.com>
2023-02-24 10:45:01 +0000

    *

--- src/getNodes.py
+++ src/getNodes.py
@@ -6,8 +6,8 @@ import rosnode
 
 if __name__ == '__main__':
     rospy.init_node('node_info')
-    node_list = rosnode.get_node_names()
+    node_list = rosnode.get_param_names()
     print(node_list)
-    for node in node_list:
-        node_info = rosnode.get_node_info(node)
-        print(node_info)
+    # for node in node_list:
+    #     node_info = rosnode.get_node_info(node)
+    #     print(node_info)
