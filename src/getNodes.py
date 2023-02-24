4631fba66c37afb0b93788143cf2bab0be1b179f
alf767443 <andreluisfranca98@gmail.com>
2023-02-24 10:00:12 +0000

    *

--- src/getNodes.py
+++ src/getNodes.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
 import rospy
-from rosnode import get_node_names
+import rosnode
+from rosgraph import Master, Node
 
 if __name__ == '__main__':
-    rospy.init_node('node_list')
-    node_names = get_node_names()
+    rospy.init_node('node_info')
+    master = Master('/rostopic')
+    node_names = rosnode.get_node_names()
     print("Nodes currently running:\n{}".format("\n".join(node_names)))
+
+    for node_name in node_names:
+        try:
+            node_info = Node(node_name, master)
+            print("Node name: {}\nURI: {}\nXML-RPC URI: {}\nPublications: {}\nSubscriptions: {}\nServices: {}\n"
+                  .format(node_name, node_info.uri, node_info.xmlrpc_uri, node_info.publications,
+                          node_info.subscriptions, node_info.services))
+        except:
+            pass
