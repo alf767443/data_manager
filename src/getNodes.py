84c1da30858a868c330f4d7f113719f001455624
alf767443 <andreluisfranca98@gmail.com>
2023-02-24 10:15:35 +0000

    *

--- src/getNodes.py
+++ src/getNodes.py
@@ -1,26 +1,8 @@
 #!/usr/bin/env python3
 
 import rospy
-from rospy import Message
-from rostopic import get_topic_class
-from rosgraph_msgs.msg import Log
-
-def callback(msg: Message):
-    if msg.name == "/rosout":
-        if msg.level == Log.INFO and "registered as a publisher" in msg.msg:
-            parts = msg.msg.split(" ")
-            node_name = parts[0]
-            topic_name = parts[-1]
-            topic_type = get_topic_class(topic_name)[0].__name__
-            print(f"Publisher node '{node_name}' is publishing to topic '{topic_name}' of type '{topic_type}'")
-        elif msg.level == Log.INFO and "subscribing to topic" in msg.msg:
-            parts = msg.msg.split(" ")
-            node_name = parts[0]
-            topic_name = parts[-1]
-            topic_type = get_topic_class(topic_name)[0].__name__
-            print(f"Subscriber node '{node_name}' is subscribing to topic '{topic_name}' of type '{topic_type}'")
 
 if __name__ == '__main__':
     rospy.init_node('node_info')
-    rospy.Subscriber('/rosout', Log, callback)
-    rospy.spin()
+    node_names = rospy.get_published_topics()
+    print("Nodes and Topics currently running:\n{}".format("\n".join([str(x) for x in node_names])))
