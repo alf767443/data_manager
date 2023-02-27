4456d2ecc204900a2e1f017c812e91167ccbdd7a
alf767443 <andreluisfranca98@gmail.com>
2023-02-27 13:24:31 +0000

    *

--- runROSnodes.sh
+++ runROSnodes.sh
@@ -3,26 +3,23 @@
 # init and sinc
 
 # Clean
-# rosnode kill /getBattery
-# rosnode kill /getMotors
-# rosnode kill /getPosition
-# rosnode kill /getPositionOdom
 rosnode kill /listenNodes
 rosnode kill /bufferManager
 rosnode kill /getSignal
 rosnode kill /queueActions
-
-
-
-#Run  gets
-# rosrun data_manager getBattery.py &
-# rosrun data_manager getMotor.py &
-# rosrun data_manager getPositionAMCL.py &
-# rosrun data_manager getPositionOdom.py &
+rosnode kill /getNodes
+rosnode kill /platform_diagnostics
 
 # Run buffer manager
 rosrun data_manager listenNode.py &
 rosrun data_manager getSignal.py &
 # rosrun data_manager queueActions.py &
 rosrun data_manager bufferManager.py
+
+rosrun data_manager listenNode.py &
+rosrun data_manager bufferManager.py &
+rosrun data_manager getSignal.py &
+# rosrun data_manager queueAction.py &
+rosrun data_manager getNodes.py &
+rosrun data_manager diagnostic.py &
 
