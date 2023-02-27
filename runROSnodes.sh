dd3d8cd84307357f70d9d423bfb06bc51fd5ad8e
alf767443 <andreluisfranca98@gmail.com>
2023-02-27 13:28:24 +0000

    *

--- runROSnodes.sh
+++ runROSnodes.sh
@@ -1,21 +1,4 @@
 #!/usr/bin/env bash
-
-# init and sinc
-
-# Clean
-rosnode kill /listenNodes
-rosnode kill /bufferManager
-rosnode kill /getSignal
-rosnode kill /queueActions
-rosnode kill /getNodes
-rosnode kill /platform_diagnostics
-
-# Run buffer manager
-rosrun data_manager listenNode.py &
-rosrun data_manager getSignal.py &
-# rosrun data_manager queueActions.py &
-rosrun data_manager bufferManager.py
-
 rosrun data_manager listenNode.py &
 rosrun data_manager bufferManager.py &
 rosrun data_manager getSignal.py &
