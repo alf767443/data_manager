4c9a86e7cf9809129f698990ff59294c4af71e4c
alf767443 <andreluisfranca98@gmail.com>
2023-03-02 11:53:57 +0000

    Test nodes add

---
+++ src/test_nodes.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Global imports
+from GlobalSets.localSave import createFile
+from sensor_msgs.msg import BatteryState
+
+# Import librarys
+import rospy, bson, psutil
+from datetime import datetime
+
+dataPath = {
+    'dataSource': 'CeDRI_UGV', 
+    'dataBase'  : 'ImpactTests',
+    'collection': 'FiveMinutes'
+}
+
+
+class test:
+    rate = rospy.Rate(1)
+    def __init__(self, NODES) -> None:
+        rospy.init_node('testNode', anonymous=False)
+        rospy.Subscriber(name='/battery_state', data_class=BatteryState, callback_args=self.callback(), queue_size=1)
+        rospy.spin()
+        
+            
+    def callback(self, msg):
+        try:
+            data = {
+                'datetime': datetime.now(),
+                'battery_voltage': msg.voltage,
+                'cpu_perc': psutil.cpu_percent(percpu=True),
+                'memory': psutil.virtual_memory(),
+            }
+            createFile(dataPath=dataPath, content=data) 
+        except Exception as e:
+            print(e)
+        for i in range(1, 300): self.rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        test()
+    except rospy.ROSInterruptException:
+        pass
