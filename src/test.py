4ea6be4072d32ef6d7e58c0a3b9f01aa0cce0d1a
alf767443 <andreluisfranca98@gmail.com>
2023-03-07 14:55:03 +0000

    *

--- src/test.py
+++ src/test.py
@@ -2,30 +2,32 @@
 import rospy
 from std_msgs.msg import String
 
-posQueue = []
+class test:
+    posQueue = []
 
-def callback(msg):
-    data = msg.data
-    _temp = None
-    print(data, _temp, posQueue)
-    if posQueue != []:
-        _temp = posQueue.copy()
-        print(_temp)
-        _temp = _temp.pop()
-        print(_temp)
-        # return None
-    if data != _temp:
-        posQueue.append(data)
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", posQueue)
-    
-def listener():
+    def __init__(self) -> None:
+        rospy.init_node('listener', anonymous=True)
 
-    rospy.init_node('listener', anonymous=True)
+        rospy.Subscriber("chatter", String, self.callback)
 
-    rospy.Subscriber("chatter", String, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+        
+            
+    def callback(self, msg):
+        data = msg.data
+        _temp = None
+        print(data, _temp, self.posQueue)
+        if self.posQueue != []:
+            _temp = self.posQueue.copy()
+            print(_temp)
+            _temp = _temp.pop()
+            print(_temp)
+            # return None
+        if data != _temp:
+            self.posQueue.append(data)
+        rospy.loginfo(rospy.get_caller_id() + "I heard %s", self.posQueue)
 
 if __name__ == '__main__':
-    listener()
+    try:
+        test()
+    except rospy.ROSInterruptException:
+        pass
