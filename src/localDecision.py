1dd99bf4acadd73b3cb5c28cd350967b844a307d
alf767443 <andreluisfranca98@gmail.com>
2023-03-28 12:34:00 +0100

    LocalDecisionMaking

--- src/localDecision.py
+++ src/localDecision.py
@@ -5,27 +5,40 @@ from GlobalSets.localSave import createFile
 from sensor_msgs.msg import BatteryState
 
 # Import librarys
-import rospy, bson, psutil
+import rospy, bson, psutil, json
 from datetime import datetime
+from std_msgs.msg import String
+class DecisionMaking:
 
 
-
-class test:
     def __init__(self) -> None:
-        rospy.init_node('testNode', anonymous=False)
+        rospy.init_node('decisionMaking', anonymous=False)
         rospy.Subscriber(name='/battery_state', data_class=BatteryState, callback=self.callback, queue_size=1)
         
+        self.pub = rospy.Publisher('decision', String, queue_size=1)
+
         rospy.spin()
             
     def callback(self, msg):
-        rate = rospy.Rate(1)
-
         if msg.voltage < 0.3:
-            print('hello')
+            self.pub.publish(data=self.NavGo2('w1'))
         rospy.sleep(1)
+
+    def NavGo2(self, point):
+        command = {
+            'status': 0,
+            'source': 0,
+            'command': point,
+            'msg': 'std_msgs/String',
+            'topic': '/navGo2',
+            'priority': 3
+        }
+        return json.dumps(command)
+
+        
 
 if __name__ == '__main__':
     try:
-        test()
+        DecisionMaking()
     except rospy.ROSInterruptException:
         pass
